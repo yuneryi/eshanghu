12e90fadccbe0ced75d6e7af8a2ae1eca4c76a1b
yuneryi <55738918+yuneryi@users.noreply.github.com>
2019-09-24 18:08:45 +0800

    Update HttpRequestErrorException.php

--- src/Exceptions/HttpRequestErrorException.php
+++ src/Exceptions/HttpRequestErrorException.php
@@ -1,12 +1,6 @@
 <?php
 
-/*
- * This file is part of the eshanghu-pay.
- *
- * (c) XiaoTeng <616896861@qq.com>
- */
-
-namespace Eshanghu\Exceptions;
+namespace Yuneryi\Exceptions;
 
 class HttpRequestErrorException extends \Exception
 {
