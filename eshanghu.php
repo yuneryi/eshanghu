41a5359db8d06527379d3d9d65786fd6349b6603
yuneryi <55738918+yuneryi@users.noreply.github.com>
2019-09-24 18:02:32 +0800

    Update eshanghu.php

--- eshanghu.php
+++ eshanghu.php
@@ -3,6 +3,6 @@
 return [
     'app_key' => '', //用户中心开发设置中获取
     'app_secret' => '', //用户中心开发设置中获取
-    'sub_mch_id' => '',  //https://1shanghu.com/user/wechat/certification 获取
+    'sub_mch_id' => '',  //https://www.yuneryi.com/user/wechat/certification 获取
     'notify' => 'https://demo.demo/callback', //回调地址
 ];
